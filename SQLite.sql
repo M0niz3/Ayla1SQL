dc47194e38d4d0ebbadc83c5dc7826d89dc4159a
M0niz3 <134505809+M0niz3@users.noreply.github.com>
2023-10-18 09:52:19 -0300

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,15 @@
+DROP TABLE demo;
+CREATE TABLE CLIENTE
+  (ID_Cliente INT NOT NULL,
+  Nome VARCHAR(1055) NOT NULL,
+  CPF char(14) Not NULL,
+  RG char(12) NOT NULL,
+  Logradouro vachar(100) NOT NULL,
+  CEP char(9) NOT NULL,
+  UF char(2) NOT NULL,
+  Cidade varchar(30) NOT NULL,
+  Telefone char(16) NOT NULL,
+  Email vachar(355) NOT NULL,
+  CNH char(9) NOT NULL,
+  Renda float(10) NULL);
+  
